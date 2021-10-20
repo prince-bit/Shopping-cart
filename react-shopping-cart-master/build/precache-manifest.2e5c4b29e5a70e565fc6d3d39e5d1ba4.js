0bf9dc379f31e8b992a7c9dff897f3c8ed3b004c
prince-bit <52601760+prince-bit@users.noreply.github.com>
2021-10-20 21:00:37 +0530

    Add files via upload

---
+++ react-shopping-cart-master/build/precache-manifest.2e5c4b29e5a70e565fc6d3d39e5d1ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3082415e635df47524c819b48582d05f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64421429cd4dfea0cab3",
+    "url": "/static/css/main.f59648f5.chunk.css"
+  },
+  {
+    "revision": "a2a24e63ec19e34e8091",
+    "url": "/static/js/2.9eab9aab.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/static/js/2.9eab9aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64421429cd4dfea0cab3",
+    "url": "/static/js/main.c9f5426c.chunk.js"
+  },
+  {
+    "revision": "75b67e50613f7dd4cc1e",
+    "url": "/static/js/runtime-main.5a1d5d0f.js"
+  }
+]);
